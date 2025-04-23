d4f97feb3c16085ea1f63c0723234b8435d27396
cleber <cleber.risu@outlook.com>
2025-04-23 19:51:26 -0300

    implementado interface grafica

---
+++ src/edu/sudoku/serivce/EventListener.java
@@ -0,0 +1,6 @@
+package edu.sudoku.serivce;
+
+public interface EventListener {
+
+  void update(final EventEnum eventType);
+}
---
+++ src/edu/sudoku/ui/custom/button/ResetButton.java
@@ -0,0 +1,12 @@
+package edu.sudoku.ui.custom.button;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class ResetButton extends JButton {
+
+  public ResetButton(final ActionListener actionListener) {
+    this.setText("Reinicar o jogo");
+    this.addActionListener(actionListener);
+  }
+}
